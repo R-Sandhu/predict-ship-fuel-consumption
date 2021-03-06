9a45590de244893e7de7f4465a74f4ab3b3ce4fb
Ranjeet Sandhu <sandhu1130@gmail.com>
2021-03-06 16:05:32 -0500

    final report and model training script

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,21 +1,39 @@
-from make_dateset import load_csv
-import numpy as np
-from csv import reader
+import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import KFold
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import GridSearchCV
-from sklearn.linear_model import LinearRegression
-from sklearn.linear_model import Lasso
-from sklearn.linear_model import ElasticNet
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.svm import SVR
-from sklearn.pipeline import Pipeline
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.ensemble import ExtraTreesRegressor
-from sklearn.ensemble import AdaBoostRegressor
-from sklearn.metrics import mean_squared_error
+
+filePath = '../data/processed/final_features.csv'
+df = pd.read_csv(filePath, index_col='Time', parse_dates=True)
+
+# Split-out validation dataset
+dataArray = df.values
+X = dataArray[:, 1:10]
+y = dataArray[:, 0]
+validation_size = 0.2
+seed = 1
+X_train, X_validation, y_train, y_validation = train_test_split(X, y, test_size=validation_size, random_state=seed)
+
+# Test options and evaluation metric
+num_folds = 10
+seed = 1
+scoring = 'neg_mean_squared_error'
+
+scaler = StandardScaler().fit(X_train)
+rescaledX = scaler.transform(X_train)
+
+# prepare the model
+model = ExtraTreesRegressor(random_state=seed,
+                            n_estimators=100,
+                            max_features=5,
+                            min_samples_split=4,
+                            n_jobs=14)
+model.fit(rescaledX, y_train)
+
+# transform the validation dataset
+rescaledValidationX = scaler.transform(X_validation)
+predictions = model.predict(rescaledValidationX)
+# print(mean_squared_error(y_validation, predictions))
+
+
 
